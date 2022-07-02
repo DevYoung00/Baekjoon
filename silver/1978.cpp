e09544846fae21be98d87ccc168ef1896245f19e
kidzero00 <dkdud203@naver.com>
2022-07-02 20:12:12 +0900

    0702

---
+++ silver/1978.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	int n, tmp,res=0;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		bool check = true;
+		cin>>tmp;
+		if(tmp <= 1) continue;
+		for(int i=2; i<tmp; i++){
+			if(tmp%i == 0) {
+				check = false;
+				continue;
+			}  
+		}
+		if(check==true) res++;
+	}
+	cout <<res;
+}
