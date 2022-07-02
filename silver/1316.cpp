5684357d892ed953b17ed34bc197b05f6c89046c
kidzero00 <dkdud203@naver.com>
2022-07-02 19:22:19 +0900

    0702

---
+++ silver/1316.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	int n,res=0;
+	string tmp;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		char ascii[30] = {0,};
+		bool check = true;
+		cin >> tmp;
+		int len = tmp.length();
+		for(int i=0; i<len; i++){
+			if(ascii[tmp[i]-97] > 0){
+				if(tmp[i-1] && tmp[i] != tmp[i-1]) check = false;
+			}
+			ascii[tmp[i]-97]++;
+		}
+		if(check == true) res++;
+	}
+	cout<<res;
+}
