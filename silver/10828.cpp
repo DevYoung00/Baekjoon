ae60856903a7d11df4f98387b3c7cce74af7ee8f
kidzero00 <dkdud203@naver.com>
2022-07-03 14:16:24 +0900

    0703

---
+++ silver/10828.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct stack{
+	int array[];
+	int top;
+	int size;
+};
+
+void push(int arr, int n){
+	for(int i=0; i<arr)
+	
+}
+int pop(){
+	
+}
+
+int size(){
+	
+}
+
+bool isEmpty(){
+	
+}
+
+int top(){
+	
+}
+
+using namespace std;
+int main(){
+}
