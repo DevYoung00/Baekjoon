6757ef9de56986a274656476e2b1a79c655dfd35
kidzero00 <dkdud203@naver.com>
2022-07-04 15:58:09 +0900

    0704

---
+++ silver/11656.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool compare(string a, string b){
+	return a<b;
+} 
+	
+int main(){
+	string str;
+	cin >> str;
+	vector<string> v;
+	v.push_back(str); 
+	for(int i=1; i<str.length(); i++){
+		string temp = str.substr(i, str.length());
+		v.push_back(temp); 
+	}
+	sort(v.begin(), v.end(), compare);
+	for(int i=0; i<v.size(); i++){
+		cout << v[i] << "\n";
+	}
+		return 0;
+}
+
+ 
+
---
+++ silver/9012.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	string str;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		int count = 0;
+		cin >> str;
+		for(int j=0; j<str.length(); j++){
+			if(str[j] == '(') count++;
+			else if(str[j] == ')') count--;
+			if(count<0) break;	
+		}
+		if(count==0) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	
+	return 0;
+}
+
+ 
+
