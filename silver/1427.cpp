035885eddef4c6d47dcfd819f04b550fb57d8b7a
kidzero00 <dkdud203@naver.com>
2022-07-13 23:12:04 +0900

    0713

---
+++ silver/1427.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool compare(int a, int b){
+	return a>b;
+}
+
+int main(){
+	string n;
+	vector<int> arr;
+	getline(cin,n);
+	for(int i=0; n[i]!='\0'; i++){
+		int temp = n[i] - 48;
+		arr.push_back(temp);
+	
+	}
+	sort(arr.begin(),arr.end(),compare);
+	for(int i=0; n[i]!='\0'; i++){
+		cout<< arr[i];
+	}
+}
